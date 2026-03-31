b8c4b051a94782467773dba61e4aae59b16ef9ff
Sukeshsukee <sugeshsukee572@gmail.com>
2026-03-31 15:57:06 +0530

    Playwright st commit

---
+++ test.config.ts
@@ -0,0 +1,6 @@
+export class TestConfig{
+
+    URL="https://practicetestautomation.com/practice-test-login/";
+    un="student";
+    pwd="Password123"
+}
---
+++ tests/Demo.spec.ts
@@ -0,0 +1,8 @@
+import {test,expect} from "@playwright/test";
+import {TestConfig} from "../test.config.ts"
+
+test("demo test", async({page})=>{
+  const config=new TestConfig();
+    await page.goto(config.URL);
+})
+
