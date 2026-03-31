7fb21ded096d367b7ce326935e8b35d5b49a92f4
Sukeshsukee <sugeshsukee572@gmail.com>
2026-03-31 16:24:05 +0530

    nd playwright

--- tests/Demo.spec.ts
+++ tests/Demo.spec.ts
@@ -4,5 +4,9 @@ import {TestConfig} from "../test.config.ts"
 test("demo test", async({page})=>{
   const config=new TestConfig();
     await page.goto(config.URL);
-})
+});
 
+test("demo test", async({page})=>{
+  const config=new TestConfig();
+    await page.goto(config.URL);
+});
